eb901561827dbe855b5d07419737b1172ebd9624
mariam osama <mariamosama1652004@gmail.com>
2026-02-22 23:43:17 +0200

    Initial commit

---
+++ src/app/core/components/auth/profile-account/profile-account.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-account',
+  imports: [],
+  templateUrl: './profile-account.component.html',
+  styleUrl: './profile-account.component.css',
+})
+export class ProfileAccountComponent {
+
+}
